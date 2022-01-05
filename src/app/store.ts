1bfc9a42824b9a3832b9635ea222a05a6388cc5a
unknown <duy88tran@gmail.com>
2022-01-05 20:32:49 +0700

    'setup-config'

---
+++ src/app/store.ts
@@ -0,0 +1,18 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "./rootSaga";
+
+const rootReducer = combineReducers({});
+
+const sagaMiddleware = createSagaMiddleware();
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(sagaMiddleware),
+});
+
+sagaMiddleware.run(rootSaga);
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
